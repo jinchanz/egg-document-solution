c67ea5bb8766810c1e0b8a282c40a7c08fda3b49
mark-ck <muscle.mark.cheng@gmail.com>
2022-03-25 21:22:36 +0800

    feat: support dynamic config

--- config/config.default.js
+++ config/config.default.js
@@ -1,19 +1,21 @@
 'use strict';
 
+// yuqueViewer.npm: '@ablula/document-client',
+// yuqueViewer.namespace: 'ant-design/course',
+// yuqueViewer.search: '/documents/search',
+// yuqueViewer.title: 'Ant Design 实战教程',
+// yuqueViewer.prefix: '/documents',
+// yuqueViewer.onlyDoc: false,
+// yuqueViewer.noHeader: false,
+// yuqueViewer.showSearch: true,
+// yuqueViewer.showEditor: true,
+// yuqueViewer.view: '/help',
+// yuqueViewer.token: '',
+// yuqueViewer.darkMode: true,
+// yuqueViewer.lightColor: '#ffffff00',
+// yuqueViewer.blackColor: 'black',
+// yuqueViewer.logo: 'https://i.ablula.tech/ablula.png',
+
 exports.yuqueViewer = {
-  npm: '@ablula/document-client',
-  namespace: 'ant-design/course',
-  search: '/documents/search',
-  title: 'Ant Design 实战教程',
-  prefix: '/documents',
-  onlyDoc: false,
-  noHeader: false,
-  showSearch: true,
-  showEditor: true,
-  view: '/help',
-  token: '',
-  darkMode: true,
-  lightColor: '#ffffff00',
-  blackColor: 'black',
-  logo: 'https://i.ablula.tech/ablula.png',
+  client: {}
 };
